0b2bfde4005866c5ff15ecd2aeba61ed1ef4d10b
Guy Tepper <guytepper@gmail.com>
2017-11-17 01:05:00 +0200

    Added upload button

--- src/views/Autopost/Autopost.js
+++ src/views/Autopost/Autopost.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Form, Select } from 'antd';
+import { Form, Select, Button } from 'antd';
 const Option = Select.Option;
 
 function groupOptions(groups) {
   return groups.map(g => <Option key={g.id}>{g.name}</Option>);
 }
 
-function Autopost({ groups }) {
+function Autopost({ groups, uploadAlbums }) {
   if (groups.length === 0) {
     return <p>It seems like there are no groups you're managing.</p>;
   }
@@ -15,6 +15,9 @@ function Autopost({ groups }) {
       <div style={{ width: 240 }}>
         <Form.Item>
           <Select defaultValue={groups[0].name}>{groupOptions(groups)}</Select>
+        </Form.Item>
+        <Form.Item>
+          <Button onClick={uploadAlbums}>Test Upload</Button>
         </Form.Item>
       </div>
     </Form>
--- src/views/Autopost/AutopostContainer.js
+++ src/views/Autopost/AutopostContainer.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import Autopost from './Autopost';
+import assets from './assets.json';
 
 @observer
 @inject('RootStore')
 class AutopostContainer extends Component {
   state = {};
 
+  /**
+   * Upload all albums to a facebook group.
+   * @param {string} groupId - The group Id to upload the album to.
+   */
+  uploadAlbums(groupId = '351760581933597') {
+    const albums = assets.albums;
+    albums.forEach(album => {
+      console.log(`/351760581933597/albums`);
+      window.FB.api(
+        `/351760581933597/albums`,
+        'POST',
+        {
+          name: album.name,
+          message: 'Album description! It works!'
+        },
+        function(response) {
+          console.log(response);
+          if (response && !response.error) {
+            console.log(response);
+          }
+        }
+      );
+    });
+  }
+
   render() {
     const { groups } = this.props.RootStore.userStore.user;
-    return <Autopost groups={groups.data} />;
+    return <Autopost groups={groups.data} uploadAlbums={this.uploadAlbums} />;
   }
 }
 
