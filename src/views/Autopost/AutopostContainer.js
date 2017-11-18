0b092d55512c4c7519a8df6b1ba5a0c6a159181b
guytepper <guytepper@gmail.com>
2017-11-18 16:39:05 +0200

    Modified uploadPhoto method

--- src/views/Autopost/AutopostContainer.js
+++ src/views/Autopost/AutopostContainer.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import axios from 'axios';
 import Autopost from './Autopost';
 import assets from './assets.json';
 
-function getDataUri(url, callback) {
-  var image = new Image();
+function getDataUri(url) {
+  return new Promise((resolve, reject) => {
+    var image = new Image();
+    image.onload = function() {
+      var canvas = document.createElement('canvas');
+      canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
+      canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+      canvas.getContext('2d').drawImage(this, 0, 0);
+      canvas.toBlob(blob => resolve(blob));
+    };
+    image.src = url;
+  });
+}
 
-  image.onload = function() {
-    var canvas = document.createElement('canvas');
-    canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
-    canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
-
-    canvas.getContext('2d').drawImage(this, 0, 0);
-
-    // // Get raw image data
-    // callback(canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, ''));
-    canvas.toBlob(callback);
-    // // ... or get as Data URI
-    // callback(canvas.toDataURL('image/png'));
-  };
-
-  image.src = url;
+/**
+ * Uploads a photo to a group album.
+ * We need to post directly to the API since Facebook SDK doesn't make the request in the right format.
+ * @param {string} albumId - The album id to upload the photo to.
+ * @param {object} blob - The photo to upload as a blob file.
+ * @param {string} description - The photo description.
+ * @param {string} token - Facebook access token to use for the API request.
+ */
+function uploadPhoto(albumId, blob, description, token) {
+  console.log(arguments);
+  var formData = new FormData();
+  formData.append('access_token', token);
+  formData.append('source', blob);
+  formData.append('message', description);
+  console.log('ahoy!');
+  axios
+    .post(`https://graph.facebook.com/${albumId}/photos`, formData)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => console.log(err));
 }
 
 /**
@@ -49,26 +67,6 @@ function uploadAlbum(groupId, name) {
   });
 }
 
-/**
- * Uploads a photo to a group album.
- * @param {string} albumId - The album id to upload the photo to.
- * @param {string} description - The photo description.
- */
-function uploadPhoto(albumId, imgData, description) {
-  window.FB.api(
-    `/${albumId}/photos`,
-    'POST',
-    {
-      source: imgData
-    },
-    function(response) {
-      console.log(response);
-      if (response && !response.error) {
-      }
-    }
-  );
-}
-
 @observer
 @inject('RootStore')
 class AutopostContainer extends Component {
@@ -78,8 +76,9 @@ class AutopostContainer extends Component {
    * Upload all albums to a facebook group.
    * @param {string} groupId - The group Id to upload the album to.
    */
-  async uploadAlbums(groupId) {
+  uploadAlbums = async groupId => {
     const albums = assets.albums;
+    const token = this.props.RootStore.userStore.token;
 
     const promises = albums.map(album => {
       return uploadAlbum('351760581933597', album.name).then(id => {
@@ -87,21 +86,19 @@ class AutopostContainer extends Component {
         return album;
       });
     });
+
     const uploadedAlbums = await Promise.all(promises);
 
     uploadedAlbums.forEach(album => {
       console.log(album);
       album.files.forEach(file => {
-        console.log(require('../../assets/GroupsAssets/albums/Getting_Ready/01.png'));
-        getDataUri(require('../../assets/GroupsAssets/albums/Getting_Ready/01.png'), data => {
-          var fd = new FormData();
-          fd.append('source', data);
-          console.log(fd);
-          uploadPhoto('351760581933597', fd, 'it works fuck.');
+        console.log(file);
+        getDataUri(require('../../assets/GroupsAssets/albums/Getting_Ready/01.png')).then(data => {
+          uploadPhoto(album.id, data, file.description, token);
         });
       });
     });
-  }
+  };
 
   render() {
     const { groups } = this.props.RootStore.userStore.user;
