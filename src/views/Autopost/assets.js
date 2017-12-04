29e4e09f106968d4cd3f44a1aa858edbebaea3ff
Noam Biel <34247980+noambiel@users.noreply.github.com>
2017-12-04 20:17:19 +0200

    Update assets.js

--- src/views/Autopost/assets.js
+++ src/views/Autopost/assets.js
@@ -12,8 +12,12 @@ export default {
           path: require('../../assets/GroupsAssets/albums/Getting_Ready/02.jpg'),
           description:
             "Visit the group every morning.\nYou'll find the daily challenge waiting for you right here.\n\nSometimes a bit of shopping might be involved, other times you'll have to work up a little courage and go into your kitchen or out to a restaurant.ֿ\nTake this opportunity to get to know some new flavors, discover new dishes and play with your food."
-        }
-      ]
+        },
+        {
+          path: require('../../assets/GroupsAssets/albums/Getting_Ready/03.jpg'),
+          description: "Lots of things! Click here for a detailed presentation: http://www.chooseveg.com/mealplan\nBreakfast, lunch and dinner ideas: http://www.chooseveg.com/recipes\nVegans eat any food that does not contain animals or animal products. Believe it or not – that's a whole lot of food! You'll be amazed to learn how many different dishes you can make with grains, legumes, fruit, vegetables, seeds, nuts, sea vegetables and mushrooms.\nHere are some examples: Chili, black bean burritos, veggie bacon, stir-fried tofu, spaghetti with soy bolognese sauce, hummus, tofu scramble, chickpea flour omelets, vegan mac 'n' cheese, baked beans, peas with fresh herbs, lentil soup, lentil burgers, grilled vegetables, big salads, pan-fried seitan, rice paper wraps, stuffed peppers, pizza with non-dairy cheese, gnocchi with cashew cream, vegetable stew, stuffed eggplants and one of our personal favorites... falafel!"
+        },
+      ],
     }
   ]
 };
